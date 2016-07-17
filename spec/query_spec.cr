04032854b17ab8042fcfbb07032650165742ff41
Oleksii Fedorov <waterlink000@gmail.com>
2016-07-18 00:32:05 +0200

    Non working WIP

--- spec/query_spec.cr
+++ spec/query_spec.cr
@@ -1,9 +1,114 @@
 require "./spec_helper"
 
-describe Query do
-  # TODO: Write tests
+include Query::CriteriaHelper
 
-  it "works" do
-    false.should eq(true)
+module Query
+  describe CriteriaHelper do
+    it "can compare criteria to number" do
+      (criteria("age") == 21).should eq(
+        Equals.new(Criteria.new("age"), 21)
+      )
+
+      (criteria("age") != 21).should eq(
+        NotEquals.new(Criteria.new("age"), 21)
+      )
+
+      (criteria("age") < 21).should eq(
+        LessThan.new(Criteria.new("age"), 21)
+      )
+
+      (criteria("age") <= 21).should eq(
+        LessThanOrEqual.new(Criteria.new("age"), 21)
+      )
+
+      (criteria("age") > 21).should eq(
+        MoreThan.new(Criteria.new("age"), 21)
+      )
+
+      (criteria("age") >= 21).should eq(
+        MoreThanOrEqual.new(Criteria.new("age"), 21)
+      )
+    end
+
+    it "can compare criteria to a string" do
+      (criteria("name") == "John Smith").should eq(
+        Equals.new(Criteria.new("name"), "John Smith")
+      )
+    end
+
+    it "can compare criteria to another criteria" do
+      (criteria("password") == criteria("confirmation")).should eq(
+        Equals.new(Criteria.new("password"), Criteria.new("confirmation"))
+      )
+    end
+  end
+
+  describe Not do
+    it "negates query" do
+      ((criteria("password") == criteria("confirmation")).not).should eq(
+        Not.new(Equals.new(Criteria.new("password"), Criteria.new("confirmation")))
+      )
+    end
+
+    it "negates other query" do
+      ((criteria("name") != "John Smith").not).should eq(
+        Not.new(NotEquals.new(Criteria.new("name"), "John Smith"))
+      )
+    end
+
+    it "is not the same as some other query with the same type signature" do
+      ((criteria("password") == criteria("confirmation")).not).should_not eq(
+        (criteria("name") == criteria("other_name")).not
+      )
+    end
+  end
+
+  describe And do
+    it "can intersect two queries" do
+      ((criteria("age") >= 21) & (criteria("age") < 42)).should eq(
+        And.new((criteria("age") >= 21), (criteria("age") < 42))
+      )
+    end
+
+    it "is not the same as some other query with the same type signature" do
+      ((criteria("age") >= 21) & (criteria("age") < 42)).should_not eq(
+        And.new((criteria("age") >= 19), (criteria("points") < 42))
+      )
+    end
+
+    # it "can be used to append things" do
+    #   q = EmptyQuery.new
+    #   query_hash = {"number_of_dependents" => 0}
+    #
+    #   query_hash.each do |key, value|
+    #     q = q.& criteria(key) == value
+    #   end
+    #
+    #   q.should eq(
+    #     Equals.new(Criteria.new("number_of_dependents"), 0)
+    #   )
+    # end
+  end
+
+  describe Equals do
+    describe "equality" do
+      it "is equal when both arguments are same" do
+        one = Equals.new(criteria("age"), 21)
+        two = Equals.new(criteria("age"), 21)
+        one.should eq(two)
+      end
+
+      it "is not equal when first argument is not same" do
+        one = Equals.new(criteria("age"), 21)
+        two = Equals.new(criteria("strength"), 21)
+        one.should_not eq(two)
+      end
+
+      it "is not equal when second argument is not same" do
+        one = Equals.new(criteria("age"), 21)
+        two = Equals.new(criteria("age"), 42)
+        one.should_not eq(two)
+      end
+    end
   end
 end
--- src/query.cr
+++ src/query.cr
@@ -1,5 +1,134 @@
-require "./query/*"
-
 module Query
-  # TODO Put your code here
+  module CriteriaHelper
+    def criteria(name)
+      Criteria.new(name)
+    end
+  end
+
+  module MacroHelper
+    macro bi_operator(name, klass)
+      def {{name.id}}(other)
+        {{klass.id}}.new(self, other)
+      end
+    end
+  end
+
+  module EqualityHelper
+    def self.equals(left : Criteria, right : Criteria)
+      left.name == right.name
+    end
+
+    def self.equals(left : Criteria, right)
+      false
+    end
+
+    def self.equals(left, right)
+      left == right
+    end
+  end
+
+  module Query
+    abstract def inspect(io)
+
+    def not
+      Not.new(self)
+    end
+
+    include MacroHelper
+    bi_operator "&", And
+  end
+
+  class EmptyQuery
+    include Query
+
+    def inspect(io)
+      io << "EMPTY_QUERY"
+    end
+  end
+
+  class BiOperator(T)
+    include Query
+
+    getter left
+    getter right
+
+    def initialize(@left : Query, @right : T)
+    end
+
+    def ==(other : self)
+      EqualityHelper.equals(left, other.left) &&
+        EqualityHelper.equals(right, other.right)
+    end
+
+    def ==(other)
+      false
+    end
+
+    def inspect(io)
+      io << "#{self.class.name}<#{left.inspect}, #{right.inspect}>"
+    end
+  end
+
+  class Equals(T) < BiOperator(T)
+  end
+
+  class NotEquals(T) < BiOperator(T)
+  end
+
+  class LessThan(T) < BiOperator(T)
+  end
+
+  class LessThanOrEqual(T) < BiOperator(T)
+  end
+
+  class MoreThan(T) < BiOperator(T)
+  end
+
+  class MoreThanOrEqual(T) < BiOperator(T)
+  end
+
+  class And(T) < BiOperator(T)
+  end
+
+  class Not
+    include Query
+
+    getter query
+
+    def initialize(@query : Query)
+    end
+
+    def ==(other : self)
+      EqualityHelper.equals(query, other.query)
+    end
+
+    def ==(other)
+      false
+    end
+
+    def inspect(io)
+      io << "Not<#{query.inspect}>"
+    end
+  end
+
+  class Criteria
+    include Query
+
+    getter name
+
+    def initialize(@name : String)
+    end
+
+    def inspect(io)
+      io << "'#{name}'"
+    end
+
+    include MacroHelper
+    bi_operator "==", Equals
+    bi_operator "!=", NotEquals
+    bi_operator "<", LessThan
+    bi_operator "<=", LessThanOrEqual
+    bi_operator ">", MoreThan
+    bi_operator ">=", MoreThanOrEqual
+  end
 end
